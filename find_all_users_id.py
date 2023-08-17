ce15be1283b7d24494f1e06c8ef0969f278b91b1
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-17 20:54:38 +0500

    users

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -1,5 +1,5 @@
 from read_data import read_data
-
+from pprint import pprint
 def find_all_users_id(data: dict)->list:
     """ 
     This function will find all the users in the json file and return the list of users id
@@ -9,4 +9,15 @@ def find_all_users_id(data: dict)->list:
     Returns:
         list: List containing all the users id
     """
-    return
+    arr=[]
+    for i in range(len(data["messages"])):
+        try:
+            arr.append(data["messages"][i]["actor_id"])
+        except:
+            arr.append(data["messages"][i]["from_id"])
+
+    return arr
+ 
+if __name__=="__main__":
+    data=read_data('data/result.json')
+    pprint(find_all_users_id(data))
